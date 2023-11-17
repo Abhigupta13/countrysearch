d5beeeea539882e8efa8b1e895a9c5b507376690
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-11-17 11:02:07 +0530

    add endpoint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import "./index.css";
 import { Country } from "./components/Country";
+import CountryDetails from "./components/countryDetails.jsx";
+import React,{useEffect,useState} from "react";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
-  return <Country />;
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://codejudge-question-artifacts-dev.s3.amazonaws.com/q-1709/data.json");
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        console.error("Error fetching countries data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return(
+  <Router>
+  <Routes>
+    <Route exact path="/" element={<Country countries={countries} />}  />
+
+    <Route exact path="/:countryName" element={<CountryDetails countries={countries} />} />
+       
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+// import CountryDetails from "./countryDetails"
 import "./Style.css";
-export const Country = () => {
-  const [countries, setCountry] = useState([]);
+
+export const Country = ({countries}) => {
+  // const [countries, setCountry] = useState([]);
   const [data, setData] = useState([]);
   const [search, setsearch] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -16,42 +19,24 @@ export const Country = () => {
     "Europe",
     "Oceania",
   ];
-  useEffect(() => {
-    getData();
-  }, []);
-  const getData = () => {
-    setIsLoading(true);
-    axios
-      .get(
-        "https://codejudge-question-artifacts-dev.s3.amazonaws.com/q-1709/data.json"
-      )
-      .then(function (response) {
-        setCountry(response.data);
-        setIsLoading(false);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
   const handlesearch = (countries) => {
     setselected("Filter By Region");
 
     return countries.filter((country) =>
       searched.some(
         (newcountry) =>
-          country[newcountry]
-            .toString()
-            .toLowerCase()
-            .indexOf(search.toLowerCase()) > -1
+          country[newcountry].toString().toLowerCase().indexOf(search.toLowerCase()) > -1
       )
     );
   };
+
   const handleclick = () => {
     setIsLoading(true);
     let a = handlesearch(countries);
     setData(a);
     setIsLoading(false);
   };
+
   const handleChange = (e) => {
     setIsLoading(true);
     setselected(e.target.value);
@@ -60,69 +45,107 @@ export const Country = () => {
       setData(countries);
     } else {
       setData(
-        [...countries].filter((country) => country.region == e.target.value)
+        [...countries].filter((country) => country.region === e.target.value)
       );
     }
     setIsLoading(false);
   };
 
-  return isLoading ? (
-    <div>Loading............</div>
-  ) : (
+  return (
+   <>
+          {isLoading ? (
+            <div>Loading............</div>
+          ) : (
+            <div>
+              <select
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+                value={selected}
+                className="custom-select"
+                aria-label="Filter Countries By Region"
+              >
+                {selecttag.map((tag) => (
+                  <option key={tag} value={tag}>
+                    {tag}
+                  </option>
+                ))}
+              </select>
+              <input
+                className="search-input"
+                type="text"
+                placeholder="Search for a country..."
+                value={search}
+                onChange={(e) => setsearch(e.target.value)}
+              />
+              <button className="search-button" onClick={handleclick}>
+                Search
+              </button>
+              <div className="container">
+                {data.length > 0
+                  ? data.map((country, i) => {
+                      return (
+                        <Link
+                          key={i}
+                          to={`/${country.name.toLowerCase()}`}
+                          className={`country-list-${country.id}`}
+                          id="list"
+                        >
+                          <img src={country.flag} alt="" id="country_img" />
+                          <div id="textdiv">
+                            <h3>{country.name}</h3>
+                            <p>Population: {country.population}</p>
+                            <p>Region: {country.region}</p>
+                            <p>Capital: {country.capital}</p>
+                          </div>
+                        </Link>
+                      );
+                    })
+                  : countries.map((country, i) => {
+                      return (
+                        <Link
+                          key={i}
+                          to={`/${country.name.toLowerCase()}`}
+                          className={`country-list-${country.id}`}
+                          id="list"
+                        >
+                          <img src={country.flag} alt="" id="country_img" />
+                          <div id="textdiv">
+                            <h3>{country.name}</h3>
+                            <p>Population: {country.population}</p>
+                            <p>Region: {country.region}</p>
+                            <p>Capital: {country.capital}</p>
+                          </div>
+                        </Link>
+                      );
+                    })}
+              </div>
+            </div>
+          )}
+     
+    </>
+  );
+};
+
+const CountryDetails = ({ countries }) => {
+  const countryName = window.location.pathname.substring(1);
+  const country = countries.find(
+    (c) => c.name.toLowerCase() === countryName.toLowerCase()
+  );
+
+  if (!country) {
+    return <div>Country not found</div>;
+  }
+
+  return (
     <div>
-      <select
-        onChange={(e) => {
-          handleChange(e);
-        }}
-        value={selected}
-        className="custom-select"
-        aria-label="Filter Countries By Region"
-      >
-        {selecttag.map((tag) => (
-          <option key={tag} value={tag}>
-            {tag}
-          </option>
-        ))}
-      </select>
-      <input
-        className="search-input"
-        type="text"
-        placeholder="Search for a country..."
-        value={search}
-        onChange={(e) => setsearch(e.target.value)}
-      />
-      <button className="search-button" onClick={handleclick}>
-        Search
-      </button>
-      <div className="container">
-        {data.length > 0
-          ? data.map((country, i) => {
-              return (
-                <div key={i} className={`country-list-${country.id}`} id="list">
-                  <img src={country.flag} alt="" id="country_img" />
-                  <div id="textdiv">
-                    <h3>{country.name}</h3>
-                    <p>Population: {country.population}</p>
-                    <p>Region: {country.region}</p>
-                    <p>Capital: {country.capital}</p>
-                  </div>
-                </div>
-              );
-            })
-          : countries.map((country, i) => {
-              return (
-                <div key={i} className={`country-list-${country.id}`} id="list">
-                  <img src={country.flag} alt="" id="country_img" />
-                  <div id="textdiv">
-                    <h3>{country.name}</h3>
-                    <p>Population: {country.population}</p>
-                    <p>Region: {country.region}</p>
-                    <p>Capital: {country.capital}</p>
-                  </div>
-                </div>
-              );
-            })}
-      </div>
+      <img src={country.flag} alt="" id="country_img" />
+                        <div id="textdiv">
+                          <h3>{country.name}</h3>
+                          <p>Population: {country.population}</p>
+                          <p>Region: {country.region}</p>
+                          <p>Capital: {country.capital}</p>
+                        </div>
     </div>
   );
 };
